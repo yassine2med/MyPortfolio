4994fba8f165ec16ed35a91385103ce8e22359a8
yassine2med <72286417+yassine2med@users.noreply.github.com>
2025-02-03 21:58:07 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,6 @@
-// filepath: /C:/Users/Yassi/Desktop/My portfolio/Portfolio Yassine/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'aceternity.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.aceternity.com',
-      },
-    ],
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
